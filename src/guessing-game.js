4e93a2c6a5c6ad7e0f3dc9075a4cd41d362c748a
Michael5366 <qwentin536@gmail.com>
2024-09-25 02:44:34 +0300

    feat: end task (Wed, Sep 25, 2024 02:44:34 AM)

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+  
     guess() {
-
+      this.isGuess = Math.round((this.min + this.max) / 2);
+      return this.isGuess;
     }
-
+  
     lower() {
-
+      this.max = this.isGuess;
     }
-
+  
     greater() {
-
+      this.min = this.isGuess;
     }
 }
 
